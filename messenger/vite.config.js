91a469bf48667206ba0217b851f26af6a2ecc3e2
Klaus <magdaevmansur@mail.com>
2024-07-12 21:11:39 +0300

    Succes commit

--- messenger/vite.config.js
+++ messenger/vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite'
-import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path'
+import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
   build: {
+    outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
-      input: {
-        main: './static/index.html',
-      },
+        input: {
+            index: resolve(__dirname, './static/index.html'),
+            register: resolve(__dirname, './src/pages/Register/RegisterPage.html'),
+            login: resolve(__dirname, './src/pages/Login/loginPage.html'),
+            404: resolve(__dirname, './src/pages/Error/404/404.html'),
+            500: resolve(__dirname, './src/pages/Error/500/500.html'),
+            profile: resolve(__dirname, './src/pages/Profile/ProfileData/ProfilePage.html'),
+            profileEditData: resolve(__dirname, './src/pages/Profile/EditData/EditDataPage.html'),
+            profileChangePassword: resolve(__dirname, './src/pages/Profile/ChangePassword/ChangePassword.html'),
+            chat: resolve(__dirname, './src/pages/Chat/ChatPage.html'),
+        },
     },
-  },
+},
   plugins: [
     handlebars({
         partialDirectory: ['./src/partials', './src/components'],
@@ -18,4 +28,4 @@ export default defineConfig({
     open: './static/index.html',
     port: 3000,
   },
-});
+})
